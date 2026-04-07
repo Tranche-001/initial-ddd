26b83c0c6b48bafec7b452f422f904a9e280179f
Tranche-001 <tranche100@gmail.com>
2026-04-07 15:53:34 -0300

    testing database connection

--- backend/studyRats.Service.Platform.Api/Program.cs
+++ backend/studyRats.Service.Platform.Api/Program.cs
@@ -1,5 +1,9 @@
 ﻿
-using studyRats.Library.Framework;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using studyRats.Service.Platform.Data;
 
 // See https://aka.ms/new-console-template for more information
 
@@ -7,3 +11,8 @@ Console.WriteLine("Hello, World!");
 
 
 var builder = WebApplication.CreateBuilder(args);
+
+
+var connectionString = builder.Configuration.GetConnectionString("DevDatabase");
+
+builder.Services.AddDbContext<DataContext>(options => options.UseSqlServer())
